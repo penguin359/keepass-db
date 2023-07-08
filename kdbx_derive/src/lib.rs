b582640521cfc66b86f9c3bdaec2e51d30235be0
Loren M. Lang <lorenl@north-winds.org>
2023-07-08 15:22:44 -0700

    Initial parsing of structs in derive macros implemented

--- kdbx_derive/src/lib.rs
+++ kdbx_derive/src/lib.rs
@@ -15,12 +15,55 @@ mod tests {
     }
 }
 
+#[derive(Debug)]
+struct KdbxField {
+    name: String,
+    r#type: String,
+}
+
 #[proc_macro_derive(KdbxParse, attributes(kdbx))]
 pub fn derive_deserializer(input: TS1) -> TS1 {
+    let ast: syn::DeriveInput = syn::parse(input).expect("bad parsing");
+    let name = &ast.ident;
+    let attrs = &ast.attrs;
+    let data = &ast.data;
+
+    let _impl_block = match *data {
+        syn::Data::Struct(ref data_struct) => {
+            // todo!("unfinished");
+            // eprintln!("Fields: {:?}", &data_struct.fields);
+            let v = data_struct.fields.iter().map(|field| {
+                eprintln!("Fields inside");
+                // todo!("unfinished");
+                let field = field.clone();
+                let name = field.ident.unwrap().to_string();
+                match field.ty {
+                syn::Type::Path(ref p) => {
+                    let r#type = p.path.segments.last().unwrap().ident.to_string();//.as_str();
+                    // format!("Support Path {} => {}", name, r#type)
+                    match r#type {
+                        _ => { KdbxField {
+                            name,
+                            r#type,
+                        }},
+                    }
+                }
+                _ => {
+                    //unimplemented!("Odd type: {:?}", field.ty);
+                    unimplemented!("Odd type");
+                },
+            }}).collect::<Vec<KdbxField>>();
+            eprintln!("Fields done: {:?}.", &v);
+            v
+        },
+        _ => {
+            unimplemented!();
+        }
+    };
     //TS1::new()
     quote! {
         fn test() {
-            println!("Hello, derive");
+            println!("Hello, derive: {:?}", "boom");
         }
     }.into()
 }
