e5f511f2689f13ea74719275b5b0e1719e23da3c
Loren M. Lang <lorenl@north-winds.org>
2023-08-15 23:28:50 -0700

    Added in a thin wrapper to run the library code and an integration test

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use std::io;
+use kdbx::lib_main;
+
+fn main() -> io::Result<()> {
+    lib_main()
+}
---
+++ tests/dump-kdbx41.rs
@@ -0,0 +1,10 @@
+use std::{process::Command, path::PathBuf};
+
+#[test]
+fn main() {
+    assert!(Command::new(env!("CARGO_BIN_EXE_kdbx"))
+        .env("KDBX_PASSWORD","asdf")
+        .arg(PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("testdata").join("dummy-kdbx41.kdbx"))
+        .status()
+        .expect("failed to execute process").success());
+}
