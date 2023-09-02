4b63c4d7ee97bcfe05413cc7c0f03dc750a47e7f
Loren M. Lang <lorenl@north-winds.org>
2023-09-02 02:39:09 -0700

    Implemented positionable stream cipher support
    This will allow decrypting fields only as needed instead of leaving
    protected data open in memory on database load.

--- src/protected_stream.rs
+++ src/protected_stream.rs
@@ -40,8 +40,8 @@ impl CipherValue {
             Self::Null => (),
             Self::Salsa20(c) => c.try_seek(pos as u64).unwrap(),
             Self::ChaCha20(c) => c.try_seek(pos as u64).unwrap(),
-            // Self::ChaCha20(c) => c.seek(pos as u64),
         }
+        self.apply_keystream(buf);
     }
 }
 
@@ -53,29 +53,23 @@ enum Error {
     InvalidCipher(u32),
 }
 
-fn new_stream(cipher: u32, p_key: &[u8]) -> Result<CipherValue, Error> {
+fn new_stream(cipher: u32, key: &[u8]) -> Result<CipherValue, Error> {
     let r#type = CipherType::from_u32(cipher).ok_or(Error::InvalidCipher(cipher))?;
-    // assert!(inner_cipher == 2 || inner_cipher == 3); // Salsa20 or ChaCha20
-    // let p_key = &inner_tlvs[&0x02u8];
-    // println!("p_key: {}", p_key.len());
-    // if cipher_opt.is_none() {
     Ok(match r#type {
         CipherType::Null => CipherValue::Null,
         CipherType::RC4 => unimplemented!(),
         CipherType::Salsa20 => {
-            //let nonce = Vec::from_hex("E830094B97205D2A").unwrap();
             let nonce = hex!("E830094B97205D2A");
-            let mut p_context = Context::new(&SHA256);
-            p_context.update(p_key);
-            let p2_key = p_context.finish().as_ref().to_owned();
+            let mut context = Context::new(&SHA256);
+            context.update(key);
+            let p2_key = context.finish().as_ref().to_owned();
             let key = Salsa20_Key::from_slice(&p2_key[0..32]);
             CipherValue::Salsa20(Salsa20::new(&key, &nonce.into()))
         }
         CipherType::ChaCha20 => {
-            let mut p_context = Context::new(&SHA512);
-            p_context.update(p_key);
-            let p2_key = p_context.finish().as_ref().to_owned();
-            println!("p2_key: {}", p2_key.len());
+            let mut context = Context::new(&SHA512);
+            context.update(key);
+            let p2_key = context.finish().as_ref().to_owned();
             let key = GenericArray::from_slice(&p2_key[0..32]);
             let nonce = GenericArray::from_slice(&p2_key[32..32 + 12]);
             CipherValue::ChaCha20(ChaCha20::new(&key, &nonce))
@@ -87,66 +81,134 @@ fn new_stream(cipher: u32, p_key: &[u8]) -> Result<CipherValue, Error> {
 mod tests {
     use super::*;
 
-    const CHACHA20_KEY: [u8; 64]  = hex!("47d3d820a2eb2b5b0b57e3397875c5fbef0676f0f9425b5f0a9ba9f32060134e9a612a5b3be2366f0fab2c8f16980760c82e194a800c0c60c2f9000d5a64daab");
+    #[test]
+    fn test_null() {
+        let mut c = new_stream(0, &[]).unwrap();
+        let mut ciphertext = [0x61, 0x62, 0x63, 0x64];
+        let expected = "abcd";
+        c.apply_keystream(&mut ciphertext);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+
+        c.apply_keystream_pos(&mut ciphertext, 10);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+
+        c.apply_keystream_pos(&mut ciphertext, 0);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+    }
+
+    const CHACHA20_KEY: [u8; 64] = hex!(
+        "47d3d820a2eb2b5b0b57e3397875c5fb"
+        "ef0676f0f9425b5f0a9ba9f32060134e"
+        "9a612a5b3be2366f0fab2c8f16980760"
+        "c82e194a800c0c60c2f9000d5a64daab");
+
+    const CHACHA20_OFFSET: usize = 0;
+    const CHACHA20_CIPHERTEXT: [u8; 8] = [
+        0x07,
+        0x69,
+        0xE8,
+        0xD6,
+        0x95,
+        0x5F,
+        0x4D,
+        0x82,
+    ];
+    const CHACHA20_PLAINTEXT: &str = "Password";
+
+    const CHACHA20_CIPHERTEXT2: [u8; 10] = [
+        0x3C,
+        0xBC,
+        0xB1,
+        0xB5,
+        0x08,
+        0xD3,
+        0x1A,
+        0x65,
+        0xD0,
+        0x52,
+    ];
+    const CHACHA20_OFFSET2: usize = 94;
+    const CHACHA20_PLAINTEXT2: &str = "don't tell";
 
     #[test]
     fn test_chacha20() {
         let mut c = new_stream(3, &CHACHA20_KEY).unwrap();
-        let mut ciphertext = [
-            0x07,
-            0x69,
-            0xE8,
-            0xD6,
-            0x95,
-            0x5F,
-            0x4D,
-            0x82,
-        ];
-        let expected = "Password";
+        let mut ciphertext = CHACHA20_CIPHERTEXT.clone();
+        let expected = CHACHA20_PLAINTEXT;
         c.apply_keystream(&mut ciphertext);
-        let actual = String::from_utf8(ciphertext.to_vec()).unwrap();
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
         assert_eq!(actual, expected);
     }
 
-// Protected Value Ciphertext: [
-//     0x3C,
-//     0xBC,
-//     0xB1,
-//     0xB5,
-//     0x08,
-//     0xD3,
-//     0x1A,
-//     0x65,
-//     0xD0,
-//     0x52,
-// ] (+94)
-// Protected Value: "don't tell"
-    const SALSA20_KEY: [u8; 32] = hex!("578b10cfc954562053f926dfdbfa26d17edc7c7e5f7bedeff88ecc22a8469a08");
+    #[test]
+    fn test_chacha20_offsets() {
+        let mut c = new_stream(3, &CHACHA20_KEY).unwrap();
+        let mut ciphertext = CHACHA20_CIPHERTEXT2.clone();
+        let expected = CHACHA20_PLAINTEXT2;
+        c.apply_keystream_pos(&mut ciphertext, CHACHA20_OFFSET2);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+
+        let mut c = new_stream(3, &CHACHA20_KEY).unwrap();
+        let mut ciphertext = CHACHA20_CIPHERTEXT.clone();
+        let expected = CHACHA20_PLAINTEXT;
+        c.apply_keystream_pos(&mut ciphertext, CHACHA20_OFFSET);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+    }
+
+    const SALSA20_KEY: [u8; 32] = hex!(
+        "578b10cfc954562053f926dfdbfa26d1"
+        "7edc7c7e5f7bedeff88ecc22a8469a08");
+
+    const SALSA20_OFFSET: usize = 0;
+    const SALSA20_CIPHERTEXT: [u8; 6] = [
+        0x10,
+        0xE8,
+        0xFC,
+        0x22,
+        0xCF,
+        0xE4,
+    ];
+    const SALSA20_PLAINTEXT: &str = "hidden";
+
+    const SALSA20_CIPHERTEXT2: [u8; 5] = [
+        0x70,
+        0x8C,
+        0x76,
+        0xA0,
+        0xF8,
+    ];
+    const SALSA20_OFFSET2: usize = 12;
+    const SALSA20_PLAINTEXT2: &str = "value";
 
     #[test]
     fn test_salsa20() {
         let mut c = new_stream(2, &SALSA20_KEY).unwrap();
-        let mut ciphertext = [
-            0x10,
-            0xE8,
-            0xFC,
-            0x22,
-            0xCF,
-            0xE4,
-        ];
-        let expected = "hidden";
+        let mut ciphertext = SALSA20_CIPHERTEXT.clone();
+        let expected = SALSA20_PLAINTEXT;
         c.apply_keystream(&mut ciphertext);
-        let actual = String::from_utf8(ciphertext.to_vec()).unwrap();
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
         assert_eq!(actual, expected);
     }
 
-//     Protected Value Ciphertext: [
-//     0x70,
-//     0x8C,
-//     0x76,
-//     0xA0,
-//     0xF8,
-// ] (+12)
-// Protected Value: "value"
+    #[test]
+    fn test_salsa20_offsets() {
+        let mut c = new_stream(2, &SALSA20_KEY).unwrap();
+        let mut ciphertext = SALSA20_CIPHERTEXT2.clone();
+        let expected = SALSA20_PLAINTEXT2;
+        c.apply_keystream_pos(&mut ciphertext, SALSA20_OFFSET2);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
 
+        let mut c = new_stream(2, &SALSA20_KEY).unwrap();
+        let mut ciphertext = SALSA20_CIPHERTEXT.clone();
+        let expected = SALSA20_PLAINTEXT;
+        c.apply_keystream_pos(&mut ciphertext, SALSA20_OFFSET);
+        let actual = String::from_utf8(ciphertext.to_vec()).expect("Valid utf-8");
+        assert_eq!(actual, expected);
+    }
 }
